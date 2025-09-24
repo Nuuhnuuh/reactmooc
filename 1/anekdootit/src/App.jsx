75c5f668930d35b96f8a27d90ac8562d285c7723
tux <privacy.disable@outlook.com>
2025-09-24 11:39:34 +0300

    2

--- 1/anekdootit/src/App.jsx
+++ 1/anekdootit/src/App.jsx
@@ -1,34 +1,62 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = (props) => {
+  const { text, onClick } = props
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
 
+const Anecdote = (props) => {
+  const { anecdote, votes, text } = props
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>{text}</h1>
+    {anecdote}
+    <br></br>
+    {votes} votes
     </>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+
+  const [selected, setSelected] = useState(0)
+  const [max, setMax] = useState(0)
+
+  const randomizeAnecdote = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+  }
+
+  const vote = () => {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+    const maxIndex = votes.reduce((maxIdx, val, idx, arr) => {
+      return val > arr[maxIdx] ? idx : maxIdx;
+    });
+    setMax(maxIndex);
+  }
+
+  return (
+    <div>
+    <Anecdote text="Anecdote of the day" anecdote={anecdotes[selected]} votes={votes[selected]} />
+    <Button text="next anecdote" onClick={() => { randomizeAnecdote(); }} />
+    <Button text="vote" onClick={() => { vote();  }} />
+    <Anecdote text="Anecdote with most votes" votes={votes[max]} anecdote={anecdotes[max]} />
+    </div>
   )
 }
 
