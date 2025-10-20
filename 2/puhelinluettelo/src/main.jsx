add61366a64f451250f3a773cfebcab002a33c3d
tux <privacy.disable@outlook.com>
2025-10-20 18:47:18 +0300

    3

--- 2/puhelinluettelo/src/main.jsx
+++ 2/puhelinluettelo/src/main.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios'
+const baseUrl = 'http://localhost:3001/api/persons'
 
-axios.get('http://localhost:3001/persons').then(response => {
-  const notes = response.data
-  console.log(notes)
+axios.get(baseUrl).then(response => {
+  const persons = response.data
+  console.log(persons)
 })
 
 import { StrictMode } from 'react'
@@ -11,5 +12,5 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <App persons={[]}/>
 )
