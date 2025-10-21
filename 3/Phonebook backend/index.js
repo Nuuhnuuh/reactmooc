04ab916425c2039e6187186061d73cfbc8ce2b20
tux <privacy.disable@outlook.com>
2025-10-21 15:40:49 +0300

    debuggausta

--- 3/Phonebook backend/index.js
+++ 3/Phonebook backend/index.js
@@ -66,6 +66,7 @@ const generateId = () => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
+    console.log(body);
 
     if (!body.name || !body.number) {
         return response.status(400).json({
